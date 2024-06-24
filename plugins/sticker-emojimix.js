7e08c43d6a238585e6d40520a614800c4a3eecc5
toylocker <132785511+toylocker@users.noreply.github.com>
2024-06-24 22:45:35 +0100

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -1,25 +1,32 @@
-import { sticker } from '../lib/sticker.js'
-import MessageType from '@adiwajshing/baileys'
-import fetch from 'node-fetch'
-import fs from "fs"
-let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw '*باستخدام هذا الأمريجب ان يكون #دمج <ايموجي 1>+<ايموجي 2>*\n*مثال:*\n*.دمج 🤨+😣*'
-let [emoji1, emoji2] = text.split`+`
-let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
-for (let res of anu.results) {
-let stiker = await sticker(false, res.url, global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
-}}
-handler.help = ['دمج'].map(v => v + ' emot1|emot2>')
-handler.tags = ['fun']
-handler.command = /^(دمج)$/i
-export default handler
-const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-fetch(url, options)
-.then(response => response.json())
-.then(json => {
-resolve(json)
-})
-.catch((err) => {
-reject(err)
-})})
+import { sticker } from '../lib/sticker.js';
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text }) => {
+    if (!text.includes('+')) throw '*باستخدام هذا الأمر يجب أن يكون #دمج <ايموجي 1>+<ايموجي 2>*\n*مثال:*\n*.دمج 🤨+😣*';
+    
+    const [emoji1, emoji2] = text.split('+').map(e => e.trim());
+    
+    try {
+        const url = `https://tenor.googleapis.com/v2/featured?key=YOUR_API_KEY&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`;
+        
+        const response = await fetch(url);
+        const json = await response.json();
+        
+        if (!json.results || json.results.length === 0) {
+            throw 'لم يتم العثور على نتائج للرموز التعبيرية المقدمة.';
+        }
+        
+        for (let res of json.results) {
+            const stickerData = await sticker(false, res.media[0].gif.url, global.packname, global.author);
+            conn.sendFile(m.chat, stickerData, null, { asSticker: true });
+        }
+    } catch (error) {
+        throw `حدث خطأ: ${error}`;
+    }
+};
+
+handler.help = ['دمج <emot1>+<emot2>'];
+handler.tags = ['fun'];
+handler.command = /^(دمج)$/i;
+
+export default handler;
